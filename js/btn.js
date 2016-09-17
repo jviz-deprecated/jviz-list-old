33d89aa81ddee5c055404eaedd1100f75e73875f
jmjuanes <josemijuanes@gmail.com>
2016-09-17 17:36:22 +0200

    js/btn.js: added method to parse the buttons list

--- js/btn.js
+++ js/btn.js
@@ -1,34 +1,27 @@
 //Add a new button
-jviz.modules.simpleList.prototype.addBtn = function(obj)
+jviz.modules.simpleList.prototype.btn = function(list)
 {
   //Check the object
-  if(typeof obj !== 'object'){ return this; }
+  if(typeof list !== 'object'){ return this; }
 
   //Check for array
-  if(jviz.is.array(obj) === false){ obj = [ obj ]; }
+  if(jviz.is.array(list) === false){ list = [ list ]; }
 
-  //Save this
-  var self = this;
-
-  //Parse the list
-  obj = this.parseBtn(obj);
-
-  //Read all the buttons
-  obj.forEach(function(el)
+  //Read the full list
+  list = list.map(function(el)
   {
-    //Read all the elements
-    for(var i = 0; i < self._data.src.length; i++)
-    {
-      //Get the cell id
-      var id = self._cell.id + '-' + i + '-cell2';
+    //Check the button id
+    if(typeof el.id === 'undefined'){ el.id = jviz.utils.genID({ length: 5 }); }
 
-      //Add the button
-      self.displayBtn(el, id, i);
-    }
+    //Check the value
+    if(typeof el.value === 'undefined'){ el.value = 'Button'; }
+
+    //Return the element
+    return el;
   });
 
-  //Concat the lists
-  this._btn.src = this._btn.src.concat(obj);
+  //Save the list
+  this._btn.src = list;
 
   //Return this
   return this;
@@ -43,31 +36,8 @@ jviz.modules.simpleList.prototype.displayBtn = function(el, cell_id, cell_index)
   //Get the button id
   var id = this._btn.id + '-' + cell_index + '-' + el.id;
 
-  //Get the button value
-  var value = el.text;
-
   //Add the button
-  jviz.dom.append({ _tag: 'div', id: id, class: this._btn.class, _html: value }, cell_id);
-};
-
-//Parse the buttons list
-jviz.modules.simpleList.prototype.parseBtn = function(list)
-{
-  //Read the full list
-  list = list.map(function(el)
-  {
-    //Check the button id
-    if(typeof el.id === 'undefined'){ el.id = jviz.utils.genID({ length: 5 }); }
-
-    //Check the value
-    if(typeof el.text === 'undefined'){ el.text = 'Button'; }
-
-    //Return the element
-    return el;
-  });
-
-  //Return the list
-  return list;
+  jviz.dom.append({ _tag: 'div', id: id, class: this._btn.class, _html: el.value }, cell_id);
 };
 
 //Add the button events
