585380cccc72580b98bae0efa544fd51adedb50b
jmjuanes <josemijuanes@gmail.com>
2016-09-17 17:36:38 +0200

    js/build.js: fixed build

--- js/build.js
+++ js/build.js
@@ -7,12 +7,15 @@ jviz.modules.simpleList.prototype.build = function()
   //Build the table
   jviz.dom.append({ _tag: 'div', id: this._table.id, class: this._table.class }, this._id);
 
+  //Parse the buttons list
+  this.btn(this._btn.src);
+
   //Get the data
   if(typeof this._data.ajax.url === 'string'){ return this.ajax(this._data.ajax); }
 
-  //Parse the buttons list
-  this._btn.src = this.parseBtn(this._btn.src);
+  //Parse the data
+  this.data(this._data.src);
 
-  //Display the data
-  return this.data(this._data.src);
+  //Draw the table
+  return this.draw();
 };
