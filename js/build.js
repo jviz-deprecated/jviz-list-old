59c62af9561fec056db6b7a0dedea059f3d05071
jmjuanes <josemijuanes@gmail.com>
2016-09-17 18:26:20 +0200

    js/build.js: minor changes

--- js/build.js
+++ js/build.js
@@ -2,10 +2,10 @@
 jviz.modules.simpleList.prototype.build = function()
 {
   //Build the main container
-  jviz.dom.append({ _tag: 'div', id: this._id, class: this._class }, this._parent);
+  jviz.dom.append(this._parent, { _tag: 'div', id: this._id, class: this._class });
 
   //Build the table
-  jviz.dom.append({ _tag: 'div', id: this._table.id, class: this._table.class }, this._id);
+  jviz.dom.append(this._id, { _tag: 'div', id: this._table.id, class: this._table.class });
 
   //Parse the buttons list
   this.btn(this._btn.src);
